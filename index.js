93d59b9feb12d72888f790aa3ca11751675b48dd
Abdellatif <mohannad.abdellatif@monster.com>
2018-08-20 15:35:17 -0400

    V2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World! Mo Mo');
+  res.send('Hello World! Mo Mo V2');
 });
 var server = app.listen(3000, function () {
 	var host = server.address().address;
